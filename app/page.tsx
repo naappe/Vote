a85179859313f2d1f47b14a124cf8fe89c54f82b
naappe <naappe@gmail.com>
2026-07-20 21:06:21 +0500

    Add Next.js dashboard page

---
+++ app/page.tsx
@@ -0,0 +1,3 @@
+import AppLayout from '../layout/AppLayout';
+import DashboardContent from '../components/DashboardContent';
+export default function Page(){return <AppLayout><DashboardContent/></AppLayout>}
