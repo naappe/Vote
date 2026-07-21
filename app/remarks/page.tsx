8faaa7c8eac370d2701b382a3931c947b9ef2dc2
naappe <naappe@gmail.com>
2026-07-21 09:52:12 +0500

    Create remarks page

---
+++ app/remarks/page.tsx
@@ -0,0 +1,6 @@
+import AppLayout from '../../layout/AppLayout';
+import RemarksContent from '../../components/RemarksContent';
+
+export default function Page(){
+ return <AppLayout><RemarksContent/></AppLayout>;
+}
