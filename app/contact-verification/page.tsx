f73a8a2c0ebe69777c964cf00599a870ccd9730c
naappe <naappe@gmail.com>
2026-07-21 11:00:06 +0500

    Create contact verification page

---
+++ app/contact-verification/page.tsx
@@ -0,0 +1,6 @@
+import AppLayout from '../../layout/AppLayout';
+import AdminVerificationContent from '../../components/AdminVerificationContent';
+
+export default function Page() {
+  return <AppLayout><AdminVerificationContent /></AppLayout>;
+}
