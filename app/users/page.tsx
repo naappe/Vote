e222adede065ef61bd1190835851a4a7b9a2de34
naappe <naappe@gmail.com>
2026-07-22 20:02:45 +0500

    Add user management route

---
+++ app/users/page.tsx
@@ -0,0 +1,3 @@
+import AppLayout from '../../layout/AppLayout';
+import UserManagementContent from '../../components/UserManagementContent';
+export default function Page(){return <AppLayout><UserManagementContent/></AppLayout>}
